786a42f124abd05d08d323d9f597b3515db020df
Paolo <paologarciaang@gmail.com>
2024-05-08 22:43:52 -0700

    Add TextButton to make image clickable

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,41 @@ class BallPage extends StatelessWidget {
         title: const Text('Ask Me Anything', style: TextStyle(color: Colors.white)),
         backgroundColor: const Color.fromRGBO(26, 67, 161, 1),
       ),
+      body: Ball(),
     );
   }
 }    
 
 
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball>{
+  //   with SingleTickerProviderStateMixin {
+  // late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  // void dispose() {
+  //   _controller.dispose();
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center( 
+      child: TextButton(onPressed: () {print("Hello");},
+      child: Image.asset('images/ball1.png'))
+      );
+    
+  }
+}
