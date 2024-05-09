b06e1133c644b410c107db8541ee1a5b6316d67d
Paolo <paologarciaang@gmail.com>
2024-05-08 20:58:38 -0700

    Add appBar title and textStyle with background colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Ask Me Anything', style: TextStyle(color: Colors.white)),
+        backgroundColor: Color.fromRGBO(26, 67, 161, 1),
+      ),
+
+    );
+  }
+}    
